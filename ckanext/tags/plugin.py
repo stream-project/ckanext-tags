6d38bcce33b37a31c8db8871a9f1968e7f1d0041
TBoonX <tboonx@googlemail.com>
2021-12-02 11:32:57 +0100

    Save work

--- ckanext/tags/plugin.py
+++ ckanext/tags/plugin.py
@@ -7,11 +7,6 @@ import ckan.plugins.toolkit as tk
 
 import rdflib
 from SPARQLWrapper import SPARQLWrapper, JSON
-import ssl
-
-# Very bad code - should be removed for production!
-ssl._create_default_https_context = ssl._create_unverified_context
-# Reason is that python 2.x seems to have issues with SSL since 2021-10-01
 
 def recreate_semantic_taxonomy_tags():
     '''Create semantic_taxonomy_tags vocab and tags, if they don't exist already.
@@ -102,17 +97,6 @@ class ExampleIDatasetFormPlugin(plugins.SingletonPlugin,
     plugins.implements(plugins.IDatasetForm, inherit=False)
     plugins.implements(plugins.ITemplateHelpers, inherit=False)
 
-    # These record how many times methods that this plugin's methods are
-    # called, for testing purposes.
-    num_times_new_template_called = 0
-    num_times_read_template_called = 0
-    num_times_edit_template_called = 0
-    num_times_search_template_called = 0
-    num_times_history_template_called = 0
-    num_times_package_form_called = 0
-    num_times_check_data_dict_called = 0
-    num_times_setup_template_variables_called = 0
-
     def update_config(self, config):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
@@ -134,7 +118,7 @@ class ExampleIDatasetFormPlugin(plugins.SingletonPlugin,
     def _modify_package_schema(self, schema):
         # Add our custom semantic_taxonomy_tags metadata field to the schema.
         schema.update({
-                'semantic_taxonomy_tags': [tk.get_validator('ignore_missing'),
+                'semantic_taxonomy_tags': [tk.get_validator('tag_string_convert'),
                     tk.get_converter('convert_to_tags')('semantic_taxonomy_tags')]
                 })
         return schema
@@ -160,47 +144,7 @@ class ExampleIDatasetFormPlugin(plugins.SingletonPlugin,
         schema.update({
             'semantic_taxonomy_tags': [
                 tk.get_converter('convert_from_tags')('semantic_taxonomy_tags'),
-                tk.get_validator('ignore_missing')]
+                tk.get_validator('tag_string_convert')]
             })
 
         return schema
-
-    # These methods just record how many times they're called, for testing
-    # purposes.
-    # TODO: It might be better to test that custom templates returned by
-    # these methods are actually used, not just that the methods get
-    # called.
-
-    def setup_template_variables(self, context, data_dict):
-        ExampleIDatasetFormPlugin.num_times_setup_template_variables_called += 1
-        return super(ExampleIDatasetFormPlugin, self).setup_template_variables(
-                context, data_dict)
-
-    def new_template(self):
-        ExampleIDatasetFormPlugin.num_times_new_template_called += 1
-        return super(ExampleIDatasetFormPlugin, self).new_template()
-
-    def read_template(self):
-        ExampleIDatasetFormPlugin.num_times_read_template_called += 1
-        return super(ExampleIDatasetFormPlugin, self).read_template()
-
-    def edit_template(self):
-        ExampleIDatasetFormPlugin.num_times_edit_template_called += 1
-        return super(ExampleIDatasetFormPlugin, self).edit_template()
-
-    def search_template(self):
-        ExampleIDatasetFormPlugin.num_times_search_template_called += 1
-        return super(ExampleIDatasetFormPlugin, self).search_template()
-
-    def history_template(self):
-        ExampleIDatasetFormPlugin.num_times_history_template_called += 1
-        return super(ExampleIDatasetFormPlugin, self).history_template()
-
-    def package_form(self):
-        ExampleIDatasetFormPlugin.num_times_package_form_called += 1
-        return super(ExampleIDatasetFormPlugin, self).package_form()
-
-    # check_data_dict() is deprecated, this method is only here to test that
-    # legacy support for the deprecated method works.
-    def check_data_dict(self, data_dict, schema=None):
-        ExampleIDatasetFormPlugin.num_times_check_data_dict_called += 1
